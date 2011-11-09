502519ad922ad719805e3e887aa1557b6dc19a88
David Hatch <dhatch387@gmail.com>
2011-11-09 12:06:48 -0500

    fix for static

--- 5_1/PigLatin.java
+++ 5_1/PigLatin.java
@@ -13,14 +13,13 @@ public class PigLatin
    {
       String sentence, result, another = "y";
       Scanner scan = new Scanner (System.in);
-      PigLatinTranslator translator = new PigLatinTranslator();
       while (another.equalsIgnoreCase("y"))
       {
          System.out.println ();
          System.out.println ("Enter a sentence (no punctuation):");
          sentence = scan.nextLine();
          System.out.println ();
-         result = translator.translate (sentence);
+         result = PigLatinTranslator.translate (sentence);
          System.out.println ("That sentence in Pig Latin is:");
          System.out.println (result);
          System.out.println ();
